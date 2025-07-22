8fe75a5bf7cf1c49dbc54c0e36e4283dbb196039
Olga-0203 <lelja030685@yahoo.com>
2025-07-22 19:45:45 +0300

    Новые обновления

--- tests/auth.spec.ts
+++ tests/auth.spec.ts
@@ -1,8 +1,20 @@
-import { test, expect } from '../fixtures/auth.fixture';
+import { test, expect } from '@playwright/test';
+import { LoginPage } from '../pages/login.page';
+import { InventoryPage } from '../pages/inventory.page';
 
-test.describe('Авторизация standard_user', () => {
-  test('Успешный вход', async ({ inventoryPage }) => {
-    await inventoryPage.expectAuthorized();
+test.describe('Авторизация пользователей', () => {
+  let login: LoginPage;
+  let inventory: InventoryPage;
+
+  test.beforeEach(async ({ page }) => {
+    login = new LoginPage(page);
+    inventory = new InventoryPage(page);
+    await login.goto('/');
+  });
+
+  test('Успешная авторизация standard_user', async () => {
+    await login.login('standard_user');
+    await inventory.expectAuthorized();
   });
 });
 
